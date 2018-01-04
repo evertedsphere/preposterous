880778a7deb1e92eecae2eeed69be726e735234d
Soham Chowdhury <chow.soham@gmail.com>
2018-01-05 00:47:48 +0530

    hindent

--- lib/Types.hs
+++ lib/Types.hs
@@ -1,65 +1,128 @@
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE PatternSynonyms #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE PatternSynonyms     #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+
 module Types where
 
-import qualified Data.Text                as Text
-import           Data.Text                (Text)
 import           Data.List.NonEmpty (NonEmpty)
+import           Data.Text          (Text)
+import qualified Data.Text          as Text
 
 -- Names
+newtype Var =
+  Var Text
+  deriving (Eq, Ord, Show)
 
-newtype Var = Var Text deriving (Eq, Ord, Show)
-newtype UnifVar = UnifVar Text deriving (Eq)
-newtype SkolVar = SkolVar Text deriving (Eq)
+newtype UnifVar =
+  UnifVar Text
+  deriving (Eq)
 
-newtype DataCon = DataCon Text deriving (Eq, Ord, Show)
-newtype TyCon = TyCon Text deriving (Eq,Show)
+newtype SkolVar =
+  SkolVar Text
+  deriving (Eq)
 
-data TyVar = Unif UnifVar | Skol SkolVar deriving (Eq)
+newtype DataCon =
+  DataCon Text
+  deriving (Eq, Ord, Show)
 
-data Sym i = SymCon DataCon | SymVar Var deriving (Eq, Ord, Show)
+newtype TyCon =
+  TyCon Text
+  deriving (Eq, Show)
+
+data TyVar
+  = Unif UnifVar
+  | Skol SkolVar
+  deriving (Eq)
+
+data Sym i
+  = SymCon DataCon
+  | SymVar Var
+  deriving (Eq, Ord, Show)
 
 -- | Primitive types.
-data PrimTy = PrimInt | PrimBool
-  deriving (Show,Eq)
+data PrimTy
+  = PrimInt
+  | PrimBool
+  deriving (Show, Eq)
 
-data Ct i = CtTriv | CtConj (Ct i) (Ct i) | CtEq (Mono i) (Mono i)
+data Ct i
+  = CtTriv
+  | CtConj (Ct i)
+           (Ct i)
+  | CtEq (Mono i)
+         (Mono i)
   deriving (Eq)
-newtype Prog i = Prog [Decl i]
-  deriving Show
-data Decl i = Decl Var (Exp i) | DeclAnn Var (Poly i) (Exp i)
-  deriving Show
+
+newtype Prog i =
+  Prog [Decl i]
+  deriving (Show)
+
+data Decl i
+  = Decl Var
+         (Exp i)
+  | DeclAnn Var
+            (Poly i)
+            (Exp i)
+  deriving (Show)
 
 data Mono i
   = MonoVar TyVar
   | MonoPrim PrimTy
   | MonoList [Mono i]
-  | MonoConApp TyCon [Mono i]
-  | MonoFun (Mono i) (Mono i)
+  | MonoConApp TyCon
+               [Mono i]
+  | MonoFun (Mono i)
+            (Mono i)
   deriving (Eq)
 
-data Poly i = Forall [SkolVar] (Ct i) (Mono i)
-  deriving Show
+data Poly i =
+  Forall [SkolVar]
+         (Ct i)
+         (Mono i)
+  deriving (Show)
 
 type Tau = Mono
+
 type Sigma = Poly
-data Exp i = ESym (Sym i) | ELam Var (Exp i) | EApp (Exp i) (Exp i) | ECase (Exp i) (NonEmpty (Alt i))
-  deriving Show
 
-data Alt i = Alt DataCon [Var] (Exp i)
-  deriving Show
+data Exp i
+  = ESym (Sym i)
+  | ELam Var
+         (Exp i)
+  | EApp (Exp i)
+         (Exp i)
+  | ECase (Exp i)
+          (NonEmpty (Alt i))
+  deriving (Show)
 
-data AxiomSch i = AxiomTriv | AxiomConj (AxiomSch i) (AxiomSch i) | AxiomImp [SkolVar] (Ct i) (Ct i)
-  deriving Show
+data Alt i =
+  Alt DataCon
+      [Var]
+      (Exp i)
+  deriving (Show)
+
+data AxiomSch i
+  = AxiomTriv
+  | AxiomConj (AxiomSch i)
+              (AxiomSch i)
+  | AxiomImp [SkolVar]
+             (Ct i)
+             (Ct i)
+  deriving (Show)
 
 type Subst i = [(TyVar, Mono i)]
-type Unifier i = [(UnifVar, Mono i)]
-newtype GenCt i = GenCt (Ct i)
-  deriving Show
 
-instance Show UnifVar where showsPrec n (UnifVar v) = showString (Text.unpack v)
-instance Show SkolVar where showsPrec n (SkolVar v) = showString (Text.unpack v)
+type Unifier i = [(UnifVar, Mono i)]
+
+newtype GenCt i =
+  GenCt (Ct i)
+  deriving (Show)
+
+instance Show UnifVar where
+  showsPrec n (UnifVar v) = showString (Text.unpack v)
+
+instance Show SkolVar where
+  showsPrec n (SkolVar v) = showString (Text.unpack v)
 
 instance Show TyVar where
   showsPrec n (Unif v) = showsPrec n v
@@ -68,12 +131,14 @@ instance Show TyVar where
 instance Show (Ct i) where
   showsPrec _ CtTriv = shows ()
   showsPrec n (CtConj l r) = showsPrec n l . showString " /\\ " . showsPrec n r
-  showsPrec n (CtEq l r) = showParen (n > 9) (showsPrec 9 l . showString " ~ " . showsPrec 9 r)
+  showsPrec n (CtEq l r) =
+    showParen (n > 9) (showsPrec 9 l . showString " ~ " . showsPrec 9 r)
 
 instance Show (Mono i) where
   showsPrec n (MonoVar v) = showsPrec n v
   showsPrec n (MonoPrim p) = showsPrec n p
   showsPrec n (MonoList ms) = showList ms
-  showsPrec n (MonoConApp (TyCon con) ms) = showString (Text.unpack con) . showList ms
-  showsPrec n (MonoFun l r) = showParen (n > 0) (showsPrec 1 l . showString " -> " . shows r)
-
+  showsPrec n (MonoConApp (TyCon con) ms) =
+    showString (Text.unpack con) . showList ms
+  showsPrec n (MonoFun l r) =
+    showParen (n > 0) (showsPrec 1 l . showString " -> " . shows r)
