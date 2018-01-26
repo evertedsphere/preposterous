d8ee12fbef3589c6a93294c14eb6c51a0e49b8b5
Soham Chowdhury <chow.soham@gmail.com>
2018-01-26 17:49:10 +0530

    Fix OverloadedStrings

--- lib/Inference/Engine.hs
+++ lib/Inference/Engine.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE ConstraintKinds       #-}
+{-# LANGUAGE OverloadedStrings     #-}
 {-# LANGUAGE FlexibleContexts      #-}
 {-# LANGUAGE LambdaCase            #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
